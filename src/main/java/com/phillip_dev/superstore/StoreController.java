2c5f56e9ae547f9842c0d707a188bb8218b0074b
phillip khoza <phillipantonk@gmail.com>
2024-02-07 16:48:02 +0200

    root getmapping

--- src/main/java/com/phillip_dev/superstore/StoreController.java
+++ src/main/java/com/phillip_dev/superstore/StoreController.java
@@ -1,8 +1,17 @@
 package com.phillip_dev.superstore;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class StoreController {
+
+    @GetMapping("/")
+    public String getForm() {
+
+        return "form";
+    }
+    
     
 }
