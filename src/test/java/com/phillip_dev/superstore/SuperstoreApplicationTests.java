cdca4f56eb45bc4f6b2e9704c1efa82f2ee5446e
phillip khoza <phillipantonk@gmail.com>
2024-02-07 16:29:29 +0200

    proj init

---
+++ src/test/java/com/phillip_dev/superstore/SuperstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillip_dev.superstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
