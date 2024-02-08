eebc7b14f7ad5677f822c1f1a153d36fc5555b7d
phillip khoza <phillipantonk@gmail.com>
2024-02-08 09:23:30 +0200

    not found constant

--- src/main/java/com/phillip_dev/superstore/Constatnts.java
+++ src/main/java/com/phillip_dev/superstore/Constatnts.java
@@ -2,4 +2,5 @@ package com.phillip_dev.superstore;
 
 public class Constatnts {
     public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
+    public static final int NOT_FOUND = 404;
 }
