a31f70f41445bb3e6446e801e9bd56156f5f4506
phillip khoza <phillipantonk@gmail.com>
2024-02-08 11:29:38 +0200

    failed constant

--- src/main/java/com/phillip_dev/superstore/Constatnts.java
+++ src/main/java/com/phillip_dev/superstore/Constatnts.java
@@ -4,4 +4,5 @@ public class Constatnts {
     public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
     public static final int NOT_FOUND = 404;
     public static final String SUCCESS_STATUS = "success";
+    public static final String FAILED_STATUS = "failed";
 }
