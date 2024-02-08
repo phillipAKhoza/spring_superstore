77258c44f2cd5a211eb3f4bd9f6b5743a59d886d
phillip khoza <phillipantonk@gmail.com>
2024-02-08 09:32:03 +0200

    item check function

--- src/main/java/com/phillip_dev/superstore/StoreController.java
+++ src/main/java/com/phillip_dev/superstore/StoreController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -16,9 +17,10 @@ public class StoreController {
 
 
     @GetMapping("/")
-    public String getForm(Model model) {
+    public String getForm(Model model, @RequestParam(required = false) String id) {
         model.addAttribute("item", new Item());
         model.addAttribute("categories", Constatnts.CATEGORIES);
+        System.err.println(id);
         return "form";
     }
     @GetMapping("/inventory")
@@ -33,6 +35,17 @@ public class StoreController {
         items.add(item);
 
         return "redirect:/inventory";
+    }
+
+    public int getIndex(String id){
+        for(int x = 0; x <= items.size(); x++){
+            if(items.get(x).getId().equals(id)){
+                return x;
+            }
+        }
+        
+        return Constatnts.NOT_FOUND;
+       
     }
     
     
