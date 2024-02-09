a4917a6b69f51f5c40ef0c64a769bc4e54beb46e
phillip khoza <phillipantonk@gmail.com>
2024-02-09 18:07:16 +0200

    category validator

---
+++ src/main/java/com/phillip_dev/superstore/Validation/CategoryValidator.java
@@ -0,0 +1,12 @@
+package com.phillip_dev.superstore.Validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CategoryValidator implements ConstraintValidator<Category, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+       
+        return false;
+    }
+}
