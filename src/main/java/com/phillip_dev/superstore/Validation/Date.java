85964860735b1631abd391acc14a2c38c30120aa
phillip khoza <phillipantonk@gmail.com>
2024-02-09 18:00:35 +0200

    Field validation interfaces

---
+++ src/main/java/com/phillip_dev/superstore/Validation/Date.java
@@ -0,0 +1,17 @@
+package com.phillip_dev.superstore.Validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Payload;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Date {
+    String message() default "Invalid Data";
+	Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ src/main/java/com/phillip_dev/superstore/Validation/Price.java
@@ -0,0 +1,16 @@
+package com.phillip_dev.superstore.Validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Payload;
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Price {
+    String message() default "Invalid Data";
+	Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+}
