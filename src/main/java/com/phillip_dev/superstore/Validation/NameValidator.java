7632ec29542e7ad18af4ffc0c1876ec1e9f28142
phillip khoza <phillipantonk@gmail.com>
2024-02-09 18:07:45 +0200

    Name Validator

---
+++ src/main/java/com/phillip_dev/superstore/Validation/NameValidator.java
@@ -0,0 +1,12 @@
+package com.phillip_dev.superstore.Validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class NameValidator implements ConstraintValidator<Name, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
