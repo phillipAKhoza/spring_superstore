a56436bbfcf6e36cad8094dfa12399b9f32283f4
phillip khoza <phillipantonk@gmail.com>
2024-02-09 18:07:53 +0200

    Price Validator

---
+++ src/main/java/com/phillip_dev/superstore/Validation/PriceValidator.java
@@ -0,0 +1,12 @@
+package com.phillip_dev.superstore.Validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PriceValidator implements ConstraintValidator<Price, Double>{
+    @Override
+    public boolean isValid(Double value, ConstraintValidatorContext context) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
