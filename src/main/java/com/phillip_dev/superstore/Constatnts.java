ab0def23d7a588f4486511c76e137843633ce606
phillip khoza <phillipantonk@gmail.com>
2024-02-10 14:33:13 +0200

    remove category constant

--- src/main/java/com/phillip_dev/superstore/Constatnts.java
+++ src/main/java/com/phillip_dev/superstore/Constatnts.java
@@ -1,7 +1,6 @@
 package com.phillip_dev.superstore;
 
 public class Constatnts {
-    public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
     public static final int NOT_FOUND = 404;
     public static final String SUCCESS_STATUS = "success";
     public static final String FAILED_STATUS = "failed";
