1b14624cbc0e73764d626c5647702e2f52563021
phillip khoza <phillipantonk@gmail.com>
2024-02-10 17:18:50 +0200

    refactor business login

--- src/main/java/com/phillip_dev/superstore/StoreController.java
+++ src/main/java/com/phillip_dev/superstore/StoreController.java
@@ -1,8 +1,5 @@
 package com.phillip_dev.superstore;
 
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,14 +20,12 @@ public class StoreController {
     
     SuperStoreService superStoreService = new SuperStoreService();
 
-
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
-      
-        int index = getIndex(id);
-        model.addAttribute("item", index == Constatnts.NOT_FOUND ?  new Item(): superStoreService.getItem(index));
+        model.addAttribute("item", superStoreService.getItemById(id));
         return "form";
     }
+
     @GetMapping("/inventory")
     public String getInventory(Model model) {
         model.addAttribute("items", superStoreService.getItems());
@@ -42,39 +37,13 @@ public class StoreController {
         if(item.getDiscount() >= item.getPrice()) result.rejectValue("discount", "", "Discount cannot be more than price");
         if(result.hasErrors()) return "form";
 
-        int index = getIndex(item.getId());
-        String status =Constatnts.SUCCESS_STATUS;
-            if(index == Constatnts.NOT_FOUND){
-                superStoreService.addItem(item); 
-            }else if(within5Days(item.getDate(), superStoreService.getItem(index).getDate())){
-                superStoreService.updateItem(index, item);
-            }else{
-                status = Constatnts.FAILED_STATUS;
-            } 
-            redirectAttributes.addFlashAttribute("status", status);
-    
-        
+        String status = superStoreService.submitItem(item);
+        redirectAttributes.addFlashAttribute("status", status);
+     
         return "redirect:/inventory";
     }
 
-    public int getIndex(String id){
-        if(id != ""){
-            for(int x = 0; x < superStoreService.getItems().size(); x++){
-                if(superStoreService.getItems().get(x).getId().equals(id)){
-                    return x;
-                }
-            } 
-        }
-       
-        
-        return Constatnts.NOT_FOUND;
-       
-    }
-    public boolean within5Days(Date newDate, Date oldDate) {
-        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
-        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
-    }
+    
 
-    
-    
+
 }
--- src/main/java/com/phillip_dev/superstore/service/SuperStoreService.java
+++ src/main/java/com/phillip_dev/superstore/service/SuperStoreService.java
@@ -1,7 +1,10 @@
 package com.phillip_dev.superstore.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import com.phillip_dev.superstore.Constatnts;
 import com.phillip_dev.superstore.Item;
 import com.phillip_dev.superstore.repository.SuperStoreRepository;
 
@@ -21,6 +24,36 @@ public class SuperStoreService {
     // Read
     public Item getItem(int index){
         return superStoreRepository.getItem(index);
+    }
+    public int getIndex(String id){
+        if(id != ""){
+            for(int x = 0; x < getItems().size(); x++){
+                if(getItems().get(x).getId().equals(id)){
+                    return x;
+                }
+            } 
+        }
+        return Constatnts.NOT_FOUND;
+    }
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
+    }
+    public Item getItemById(String id){
+        int index = getIndex(id);
+        return index == Constatnts.NOT_FOUND ?  new Item(): getItem(index);
+    }
+    public String submitItem(Item item){
+        int index = getIndex(item.getId());
+        String status =Constatnts.SUCCESS_STATUS;
+            if(index == Constatnts.NOT_FOUND){
+                addItem(item); 
+            }else if(within5Days(item.getDate(), getItem(index).getDate())){
+                updateItem(index, item);
+            }else{
+                status = Constatnts.FAILED_STATUS;
+            } 
+            return status;
     }
     
 }
