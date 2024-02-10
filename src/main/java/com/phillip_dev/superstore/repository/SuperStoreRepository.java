5b4856d33f824acba7244990ec88e5ca82e6e7c5
phillip khoza <phillipantonk@gmail.com>
2024-02-10 16:10:45 +0200

    comments

--- src/main/java/com/phillip_dev/superstore/repository/SuperStoreRepository.java
+++ src/main/java/com/phillip_dev/superstore/repository/SuperStoreRepository.java
@@ -7,16 +7,19 @@ import com.phillip_dev.superstore.Item;
 
 public class SuperStoreRepository {
     private List<Item> items = new ArrayList<Item>();
-
+    // Read
     public List<Item> getItems(){
         return items;
     }
+    // Create
     public void addItem(Item item){
         items.add(item);
     }
+    // Update
     public void updateItem(int index, Item item){
         items.set(index, item);
     }
+    // Read
     public Item getItem(int index){
         return  items.get(index);
     }
