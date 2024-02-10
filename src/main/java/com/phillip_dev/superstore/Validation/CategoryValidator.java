c8ef0744646bc9de3839cbf2e1eacd4a7101ffd4
phillip khoza <phillipantonk@gmail.com>
2024-02-10 08:56:03 +0200

    category validation condition

--- src/main/java/com/phillip_dev/superstore/Validation/CategoryValidator.java
+++ src/main/java/com/phillip_dev/superstore/Validation/CategoryValidator.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintValidatorContext;
 public class CategoryValidator implements ConstraintValidator<Category, String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-       
+       if(value.equals("Choose Category")) return true;
         return false;
     }
 }
