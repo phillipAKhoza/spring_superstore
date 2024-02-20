6961874cbea95b7f8cb281307592e43b978b5b34
phillip khoza <phillipantonk@gmail.com>
2024-02-20 11:41:23 +0200

    wire as repository bean

--- src/main/java/com/phillip_dev/superstore/repository/SuperStoreRepository.java
+++ src/main/java/com/phillip_dev/superstore/repository/SuperStoreRepository.java
@@ -3,8 +3,11 @@ package com.phillip_dev.superstore.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.phillip_dev.superstore.Item;
 
+@Repository
 public class SuperStoreRepository {
     private List<Item> items = new ArrayList<Item>();
     // Read
