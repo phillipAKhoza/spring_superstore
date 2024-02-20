e1e54fb16e8fc8e8d95263f53ea229b76f6bbd42
phillip khoza <phillipantonk@gmail.com>
2024-02-20 15:01:42 +0200

    add runtwith, mock and injectmocks

--- src/test/java/com/phillip_dev/superstore/SuperStoreServiceTest.java
+++ src/test/java/com/phillip_dev/superstore/SuperStoreServiceTest.java
@@ -1,5 +1,20 @@
 package com.phillip_dev.superstore;
 
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.phillip_dev.superstore.repository.SuperStoreRepository;
+import com.phillip_dev.superstore.service.SuperStoreService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SuperStoreServiceTest {
+
+    @Mock
+    private SuperStoreRepository superStoreRepository;
+
+    @InjectMocks
+    private SuperStoreService superStoreService;
     
 }
